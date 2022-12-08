9f4f8b39a12e040244a05c80f4ef3eb8308b43da
kevinhouguet <k.houguet@gmail.com>
2022-12-08 13:33:49 +0100

    ajout d info dans les card du film dans film list

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -16,6 +16,16 @@ const mainController = {
         }
       })
     }
+    animes.map(anime => {
+      if(anime.duration % 60 === 0){
+        anime.duration = anime.duration / 60 + 'h';
+      } else {
+        const heures = Math.floor(anime.duration / 60) + 'h';
+        const minutes = (anime.duration % 60) < 10 ? '0' + anime.duration % 60 : anime.duration % 60;
+        anime.duration = heures + minutes;
+      }
+    });
+    // console.log(animes[0])
     res.render('filmsList', {animes, filter});
   },
 
